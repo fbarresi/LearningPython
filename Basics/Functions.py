7bcd8962031d8a523b0c4e7de7f65ecec8fbee80
Federico Barresi <federico.barresi@evopro-ag.de>
2019-08-07 18:05:07 +0200

    added docstring to testo function

--- Basics/Functions.py
+++ Basics/Functions.py
@@ -1,5 +1,9 @@
 def test():
+    """
+    This function show how you can write a simple python function
+    """
     print("Hey Python!")
 
 if __name__ == "__main__":
+    print(test.__doc__)
     test()
